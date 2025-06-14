83975291ec6619e88d37b5f29fc50c146fc8d2a3
Vto40 <garcivictor@hotmail.com>
2025-06-14 11:01:55 +0200

    1

--- backend/models/log.js
+++ backend/models/log.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose');
 
 const logSchema = new mongoose.Schema({
-  usuario: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true },
-  tipo: { type: String, required: true }, // Ejemplo: 'valoracion', 'login', etc.
-  descripcion: { type: String, required: true },
-  fecha: { type: Date, default: Date.now },
+  usuario: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Usuario', 
+    required: true 
+  },
+  tipo: { 
+    type: String, 
+    required: true 
+  },
+  descripcion: { 
+    type: String, 
+    required: true 
+  },
+  fecha: { 
+    type: Date, 
+    default: Date.now 
+  },
 });
 
 module.exports = mongoose.model('Log', logSchema);
