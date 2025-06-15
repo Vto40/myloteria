cf31c5172cd494857eca5bf294e2a09ec9e39821
VICTOR GARCIA <garcivictor@hotmail.com>
2025-06-15 13:14:56 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const valoracionesRouter = require('./routes/valoraciones');
+//const valoracionesRouter = require('./routes/valoraciones');
 
 app.use(express.json());
-app.use('/valoraciones', valoracionesRouter);
+//app.use('/valoraciones', valoracionesRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
